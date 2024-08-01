bdec15b33f033d4b07b5490e58ba24ffda563383
hendisantika <hendisantika@yahoo.co.id>
2024-08-01 10:16:59 +0700

    Modify page.js 🫸🌀✏️📗🐧🐳

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,7 @@
 export default function Home() {
+    // States
+    const [input, setInput] = useState(null);
+    const [response, setResponse] = useState(null);
     return (
         <div className="flex flex-col relative bg-grey font-mono items-center min-h-screen border-t-2 border-active">
             <h1 className="text-6xl font-bold text-primary mt-20">
@@ -11,6 +14,7 @@ export default function Home() {
                 type="text"
                 placeholder="Enter a link, number or any text to generate the QR Code"
                 className="mt-10 text-sm w-1/2 p-4 rounded"
+                onChange={e => setInput(e.target.value)}
             ></input>
             <button className="mt-6 p-4 bg-active hover:opacity-90 rounded text-primary font-bold inline-flex">
                 Generate QR Code
